d313c9ccb4bb239f6e7a0afe60fba58a199d1b68
vishwas <vsvishwas03@gmail.com>
2026-01-21 21:25:03 +0530

    Add Loan microservice

---
+++ loan-service/LoanMicroservice/loans/src/main/java/com/VishwasMicoservice/loans/repository/LoansRepository.java
@@ -0,0 +1,16 @@
+package com.VishwasMicoservice.loans.repository;
+
+import com.VishwasMicoservice.loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
+
+}
